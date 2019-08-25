a6533f6c008ca72155810f7297e2c213b1f50944
buharov-alexander <buharovsasha@yandex.ru>
2019-08-25 21:47:59 +0700

    Updates

---
+++ precache-manifest.ccc5d45837788679f8654c0fcb074f78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77693ceec1ab02d22db65be90cfc640a",
+    "url": "/yandex_js_course/index.html"
+  },
+  {
+    "revision": "a5f719e9787c38afe799",
+    "url": "/yandex_js_course/static/css/main.1f3b3d93.chunk.css"
+  },
+  {
+    "revision": "9a0371498236dee2e07a",
+    "url": "/yandex_js_course/static/js/2.53385645.chunk.js"
+  },
+  {
+    "revision": "a5f719e9787c38afe799",
+    "url": "/yandex_js_course/static/js/main.b3cf4c05.chunk.js"
+  },
+  {
+    "revision": "961ceb71f044d65c035c",
+    "url": "/yandex_js_course/static/js/runtime~main.f2727716.js"
+  },
+  {
+    "revision": "4906365ee1f54eeb41d3f4535c6f8bb4",
+    "url": "/yandex_js_course/static/media/tasks.4906365e.yaml"
+  },
+  {
+    "revision": "8e429045406a96352d475cd2b218720f",
+    "url": "/yandex_js_course/static/media/users.8e429045.yaml"
+  }
+]);
